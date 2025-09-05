0307d5d9fbb91da919f4b4730d6476740e4740c8
priyanshu0412 <pedvops04@gmail.com>
2025-09-05 23:34:41 +0530

    event controller updated

--- src/controller/event.controller.js
+++ src/controller/event.controller.js
@@ -37,7 +37,7 @@ const createEvent = async (req, res) => {
             createdBy: req.userId,
         });
         await event.save();
-        res.status(201).json(event);
+        res.status(200).json({ message: "Event Created Successfully", data: event });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
