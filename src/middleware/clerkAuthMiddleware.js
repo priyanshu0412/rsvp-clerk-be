302e7d861dca7b78326a85ae58c6f830bfd35781
priyanshu0412 <pedvops04@gmail.com>
2025-09-06 10:25:38 +0530

    middleware updated

--- src/middleware/clerkAuthMiddleware.js
+++ src/middleware/clerkAuthMiddleware.js
@@ -1,27 +1,25 @@
-const { verifyToken } = require('@clerk/backend');
+const { verifyToken } = require("@clerk/backend");
 require("dotenv").config();
-
-// ----------------------------------------
 
 const clerkAuthMiddleware = async (req, res, next) => {
     const authHeader = req.headers.authorization;
-    if (!authHeader) return res.status(401).json({ message: 'Missing auth header' });
+    if (!authHeader) return res.status(401).json({ message: "Missing auth header" });
 
-    const token = authHeader.split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'Invalid auth format' });
+    const token = authHeader.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "Invalid auth format" });
 
     try {
         const { payload } = await verifyToken(token, {
-            issuer: 'https://rsvp-clerk-be.onrender.com',
-            authorizedParties: ['http://localhost:3000'],
             secretKey: process.env.CLERK_SECRET_KEY,
         });
 
         req.userId = payload.sub;
         console.log("req.userId:", req.userId);
+
         next();
     } catch (err) {
-        return res.status(401).json({ message: 'Invalid token', error: err.message });
+        console.error("JWT verification failed:", err.message);
+        return res.status(401).json({ message: "Invalid token", error: err.message });
     }
 };
 
